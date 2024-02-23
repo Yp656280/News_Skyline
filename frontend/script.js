d2eb6e88996d8f7c2e0d041bbb614bf9cfe4630f
Yp656280 <112876453+Yp656280@users.noreply.github.com>
2024-02-23 09:59:40 +0530

    implemented signin

--- frontend/script.js
+++ frontend/script.js
@@ -1,54 +1,21 @@
-// const labelEmail = document.querySelector(".email");
-
-// const labelPassword = document.querySelector(".password");
-// const submit = document.querySelector(".submit");
-
-// submit.addEventListener("click", async function (e) {
-//   e.preventDefault();
-
-//   const email = labelEmail.value;
-//   const password = labelPassword.value;
-
-//   console.log(email, password);
-
-//   // const url = "https://api.example.com/data";
-
-//   const requestOptions = {
-//     method: "POST", // or 'PUT', 'GET', 'DELETE', etc.
-//     headers: {
-//       "Content-Type": "application/json", // Tell the server we are sending JSON data
-//     },
-//     body: JSON.stringify({
-//       email,
-//       password,
-//     }), // Convert the JavaScript object to JSON string
-//   };
-
-//   const req = await fetch(
-//     "http://localhost:5000/api/users/login",
-//     requestOptions
-//   );
-//   console.log(req.statusText === "Bad Request");
-//   if (req.statusText === "Bad Request") {
-//     alert("sexy");
-//   }
-//   const data = await req.json();
-//   console.log(data);
-//   if (data.title === "message") {
-//     alert("sexy");
-//   }
-// });
+const signup_name = document.querySelector(".signup_name");
+const signup_email = document.querySelector(".signup_email");
+const signup_password = document.querySelector(".signup_password");
+const signup_submit = document.querySelector(".signup_submit");
+const email_error = document.querySelector(".email-error");
+const email_password = document.querySelector(".password-error");
 document
   .querySelector(".img__btn")
   .addEventListener("click", async function () {
     document.querySelector(".cont").classList.toggle("s--signup");
   });
 
-const signup_name = document.querySelector(".signup_name");
-const signup_email = document.querySelector(".signup_email");
-const signup_password = document.querySelector(".signup_password");
-const signup_submit = document.querySelector(".signup_submit");
-const email_error = document.querySelector(".email-error");
+const signin_email = document.querySelector(".signin_email");
+const signin_password = document.querySelector(".signin_password");
+const signin_submit = document.querySelector(".signin_submit");
+
+//SIGNUP
+
 signup_submit.addEventListener("click", async function (e) {
   e.preventDefault();
 
@@ -77,10 +44,69 @@ signup_submit.addEventListener("click", async function (e) {
   );
   if (req.statusText === "Bad Request") {
     email_error.style.visibility = "visible";
+
     signup_password.value = "";
   } else {
+    // email_error.style.visibility = "hidden";
+
     signup_email.value = signup_name.value = signup_password.value = "";
   }
   const data = await req.json();
   console.log(data._id, data.email);
+});
+0;
+
+//SIGN IN
+
+signin_submit.addEventListener("click", async function (e) {
+  e.preventDefault();
+  email_error.style.visibility = "hidden";
+  email_password.style.visibility = "hidden";
+
+  const email = signin_email.value;
+  const password = signin_password.value;
+
+  console.log(email, password);
+
+  const requestOptions = {
+    method: "POST", // or 'PUT', 'GET', 'DELETE', etc.
+    headers: {
+      "Content-Type": "application/json", // Tell the server we are sending JSON data
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }), // Convert the JavaScript object to JSON string
+  };
+
+  const req = await fetch(
+    "http://localhost:5000/api/users/login",
+    requestOptions
+  );
+
+  if (req.statusText === "Bad Request") {
+    email_error.style.visibility = "visible";
+    email_password.style.visibility = "visible";
+
+    signin_password.value = "";
+  } else {
+    signin_email.value = signin_password.value = "";
+    const token = await req.json();
+    console.log(token.acessToken);
+
+    const requestOptions2 = {
+      method: "GET", // or 'PUT', 'GET', 'DELETE', etc.
+      headers: {
+        'Authorization': `Bearer ${token.acessToken}`, // prettier-ignore
+
+        "Content-Type": "application/json", // Tell the server we are sending JSON data
+      },
+    };
+    const req2 = await fetch(
+      "http://localhost:5000/api/users/current",
+      requestOptions2
+    );
+    const data = await req2.json();
+    console.log(data);
+  }
 });
