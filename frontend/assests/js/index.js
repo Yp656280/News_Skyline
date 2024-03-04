4a6491ac14050643034ce888eef74d1c79625957
Yp656280 <112876453+Yp656280@users.noreply.github.com>
2024-03-04 11:40:38 +0530

    Fix:Implemented Search functionality Changed the Flow

--- frontend/assests/js/index.js
+++ frontend/assests/js/index.js
@@ -1,77 +1,116 @@
 const urlStart = "http://localhost:3000/api/";
+
 document.addEventListener("DOMContentLoaded", function () {
-  var navLinks = document.querySelectorAll(".nav-link");
+  // nav bar array
+  const newsHeadline = [
+    "Home",
+    "India",
+    "World",
+    "Local",
+    "Business",
+    "Technology",
+    "Entertainment",
+    "Sports",
+    "Science",
+  ];
+  const navLinksContainer = document.querySelector(".navbar-nav");
 
-  var cardContainer = document.createElement("div");
-  cardContainer.classList.add("card-container");
+  // showing data in navbar
+  newsHeadline.forEach((category) => {
+    const navItem = document.createElement("li");
+    navItem.classList.add("nav-item");
 
-  navLinks.forEach(async function (link) {
-    link.addEventListener("click", async function () {
-      cardContainer.innerHTML = "";
+    const navLink = document.createElement("a");
+    navLink.classList.add("nav-link");
+    navLink.href = "#";
+    navLink.textContent = category;
 
-      const requestOptions2 = {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-      const req2 = await fetch(
-        `${urlStart}news/${link.innerText}`,
-        requestOptions2
-      );
-      const data = await req2.json();
+    navLink.addEventListener("click", () => showData(category));
 
-      for (let i = 0; i < data.articles.length; i++) {
-        const element = data.articles[i];
-
-        var card = document.createElement("div");
-        card.classList.add("card", "mb-3");
-
-        var img = document.createElement("img");
-
-        img.src = element.urlToImage;
-
-        img.width = 200;
-        img.height = 150;
-        img.alt = "Card image cap";
-
-        img.addEventListener("click", function () {
-          // Define the URL you want to open in a new tab
-          var url = element.url;
-
-          // Open the URL in a new tab
-          window.open(url, "_blank");
-        });
-        console.log("--42--", element.urlToImage);
-        // console.log(element.url);
-        var cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
-
-        var cardTitle = document.createElement("h5");
-        cardTitle.classList.add("card-title");
-        cardTitle.textContent = element.title;
-
-        var cardText = document.createElement("p");
-        cardText.classList.add("card-text");
-        cardText.textContent = element.description;
-
-        var lastUpdatedText = document.createElement("p");
-        lastUpdatedText.classList.add("card-text", "text-muted");
-        lastUpdatedText.textContent = "Last updated " + element.publishedAt;
-
-        cardBody.appendChild(cardTitle);
-        cardBody.appendChild(cardText);
-        cardBody.appendChild(lastUpdatedText);
-
-        card.appendChild(img);
-        card.appendChild(cardBody);
-
-        cardContainer.appendChild(card);
-      }
-
-      console.log("I am here");
-
-      document.body.appendChild(cardContainer);
-    });
+    navItem.appendChild(navLink);
+    navLinksContainer.appendChild(navItem);
   });
+
+  // search functionality started
+  let searchForm = document.getElementById("searchForm");
+  let searchQuery = null;
+
+  searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    searchQuery = document.getElementById("searchInput").value;
+    showData(searchQuery);
+  });
+  // search ended
+
+  //show data
+  let showData = async (value) => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const req = await fetch(`${urlStart}news/${value}`, requestOptions);
+    const data = await req.json();
+    var navLinks = document.querySelectorAll(".nav-link");
+    var cardContainer = document.querySelector(".card-container");
+
+    if (!cardContainer) {
+      cardContainer = document.createElement("div");
+      cardContainer.classList.add("card-container", "rounded");
+      cardContainer.setAttribute("data-bs-theme", "dark");
+      document.body.appendChild(cardContainer);
+    }
+
+    cardContainer.innerHTML = "";
+
+    for (let i = 0; i < data.articles.length; i++) {
+      const element = data.articles[i];
+      var card = document.createElement("div");
+      card.classList.add("card", "m-5", "p-2", "text-center", "rounded-5");
+
+      var img = document.createElement("img");
+      img.src =
+        element.urlToImage != null
+          ? element.urlToImage
+          : "assests/img/No_Image_Available.jpg";
+      img.width = 200;
+      img.height = 150;
+      img.alt = "Card image cap";
+
+      var cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+
+      var cardTitle = document.createElement("h5");
+      cardTitle.classList.add("card-title");
+      cardTitle.textContent = element.title;
+
+      var cardText = document.createElement("p");
+      cardText.classList.add("card-text");
+      cardText.textContent = element.description;
+
+      const moreDetails = document.createElement("a");
+      moreDetails.classList.add("card-link");
+      console.log(element.url);
+      moreDetails.href = element.url;
+      moreDetails.target = "_blank";
+      moreDetails.textContent = "Read More";
+
+      var lastUpdatedText = document.createElement("p");
+      lastUpdatedText.classList.add("card-text", "text-muted", "card-footer");
+      lastUpdatedText.textContent = "Last updated - " + element.publishedAt;
+
+      cardBody.appendChild(cardTitle);
+      cardBody.appendChild(cardText);
+      cardBody.appendChild(moreDetails);
+      cardBody.appendChild(lastUpdatedText);
+
+      card.appendChild(img);
+      card.appendChild(cardBody);
+
+      cardContainer.appendChild(card);
+    }
+  };
+  //end of show data
 });
